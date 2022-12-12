603fb111a7393cc645dfaf013128698487a44ece
eyho-cao <49457118+eyho-cao@users.noreply.github.com>
2022-12-11 19:48:35 -0500

    fix logic for filtering servants

--- createServantsPool.py
+++ createServantsPool.py
@@ -24,22 +24,18 @@ def drawRandomServant(servantClass):
 def filterExistingServants(unfilteredServantsPool):
     out = []
     for i in unfilteredServantsPool:
-        if(len(servantsPool) > 0):
+            isInList = False
             for j in servantsPool:
-                if(not (i.servantName == j.servantName and i.servantClass == j.servantClass)):
-                    out.append(i)
-        else:
-            out.append(i)
+                if(i.servantName == j.servantName and i.servantClass == j.servantClass):
+                    isInList = True
+            if(not isInList):
+                out.append(i)
     return out
 
 def createTestPool():
     for i in range(8):
         tempServant = Servant(i, random.choice(servantClassList))
         servantsPool.append(tempServant)
-
-def printServantPool():
-    for i in servantsPool:
-        print("Servant:", i.servantName, ' || Class:', i.servantClass)
 
 def printServant(servant):
     print("Servant: ", servant.servantName, servant.servantClass)
