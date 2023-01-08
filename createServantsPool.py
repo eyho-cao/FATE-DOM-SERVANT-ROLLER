39914bf92762bcd4b2ef77c452a3da2d12be993b
eyho-cao <49457118+eyho-cao@users.noreply.github.com>
2023-01-07 21:46:48 -0500

    more code refactor
    decoupled selecting servant class, drawing servant, and checking if servant is in list

--- createServantsPool.py
+++ createServantsPool.py
@@ -8,31 +8,56 @@ servantClassList = getClassList()
 
 def createServantsPool():
     for i in servantClassList:
-        servantsPool.append(drawRandomServant(i))
+        servantsPool.append(drawRandomServantFromClass(i, servantsPool))
     last = selectRandomClass()
-    servantsPool.append(drawRandomServant(last))
+    servantsPool.append(drawRandomServantFromClass(last, servantsPool))
 
     return servantsPool
 
 def selectRandomClass():
     return random.choice(servantClassList)
 
-def drawRandomServant(servantClass):
+def drawRandomServantFromClass(servantClass, compiledPool):
     unfilteredServantsList = readServants(servantClass)
-    uniqueServants = filterExistingServants(unfilteredServantsList)
+    uniqueServants = filterExistingServants(unfilteredServantsList, compiledPool)
     return random.choice(uniqueServants)
 
-
-def filterExistingServants(unfilteredServantsPool):
+def filterExistingServants(unfilteredServantsPool, compiledPool):
     out = []
     for i in unfilteredServantsPool:
-            isInList = False
-            for j in servantsPool:
-                if(i.servantName == j.servantName and i.servantClass == j.servantClass):
-                    isInList = True
+            isInList = isServantInPool(i, compiledPool)
             if(not isInList):
                 out.append(i)
     return out
+
+def isServantInPool(servant, pool):
+    for i in pool:
+        if(i.servantName == servant.servantName and i.servantClass == servant.servantClass):
+            return True
+
+def drawRandomServant():
+    servantClass = selectRandomClass()
+    servant = drawRandomServantFromClass(servantClass)
+    return servant
+
+def drawMiyuServants(usedPool):
+    miyuPool = []
+    for i in range(3):
+        servantUsed = True
+        servant = None
+        while servantUsed:
+            servantClass = selectRandomClass()
+            servant = drawRandomServantFromClass(servantClass)
+            if(not isServantInPool(servant, usedPool)):
+                servantUsed = False
+        if(servant):
+            miyuPool.append(servant)
+        else:
+            print('Something went wrong and no valid servant was selected')
+    return miyuPool
+
+            
+            
 
 def createTestPool():
     for i in range(8):
@@ -41,3 +66,7 @@ def createTestPool():
 
 def printServant(servant):
     print("Servant: ", servant.servantName, servant.servantClass)
+
+def printServantPool(servantsPool):
+    for i in servantsPool:
+        print("Servant:", i.servantName, ' || Class:', i.servantClass)
